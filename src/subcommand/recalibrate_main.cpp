80d84dd77e06832e43cbbe0d4e38e7ff64e505a3
jeffrey-chan <jachan.santiago@gmail.com>
2018-07-09 17:07:23 -0700

    add bag of word features to the model

--- src/subcommand/recalibrate_main.cpp
+++ src/subcommand/recalibrate_main.cpp
@@ -28,6 +28,30 @@ void help_recalibrate(char** argv) {
          << "    -t, --threads N          number of threads to use" << endl;
 }
 
+map<string, int> sequence_to_bag_of_words(string seq, int kmer){
+    map<string, int> bw;// = generate_dict(kmer);
+    map<string, int>::iterator it;
+    int val;
+    for(int i = 0; i < seq.length() - kmer; i++){
+        string current_seq = seq.substr(i, kmer);
+        it = bw.find(current_seq);
+        if(it == bw.end()){
+            bw[current_seq] = 1;
+        }else{
+            bw[current_seq] += 1;
+        }
+    }
+    return bw;
+}
+
+string bag_of_word_to_string(map<string, int> bw){
+    stringstream s;
+    for(map<string,int>::iterator i = bw.begin(); i != bw.end(); i++){
+        s << i->first << ":" << i->second << " ";
+    }
+    return s.str();
+}
+
 /// Turn an Alignment into a Vowpal Wabbit format example line.
 /// If train is true, give it a label so that VW will train on it.
 /// If train is false, do not label the data.
@@ -63,6 +87,7 @@ string alignment_to_example_string(const Alignment& aln, bool train) {
     s << "identity:" << aln.identity() << " ";
     
     // TODO: more features
+    s << bag_of_word_to_string(sequence_to_bag_of_words(aln.sequence(), 10));
     return s.str();
 }
 
